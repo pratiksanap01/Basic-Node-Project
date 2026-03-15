5ac0ca1be47e65c86d9492f06cfe140ae7912dce
pratiksanap01 <sanappratik07@gmail.com>
2026-03-15 17:44:22 +0530

    Add Methods

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ app.get("/api/users", (req, res) => {
     return res.json(users)
 })
 
-app.get("/api/users/:id", (req, res) => {
+app
+.route
+.get((req, res) => {
     const id = Number(req.params.id)
     const user = users.find((user) => user.id === id)
      return res.json(user)
-});
+})
+.
+
+
+app.post("/api/users", (req, res) => {
+    return res.json({status : "pending"})
+})
 
 
 
